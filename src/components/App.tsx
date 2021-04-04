7d362c4d3fb73d957a0aba56f533c9077a22c808
Gustavo Ushjima <lgu85@hotmail.com>
2021-04-04 17:38:56 -0300

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+import './App.global.css'
+
+import Login from './Login'
+
+export default function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" component={Login} />
+            </Switch>
+        </Router>
+    )
+}
---
+++ src/components/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../App.global.css'
+
+export default function Login() {
+    return (
+        <div>
+            <h2>Login</h2>
+        </div>
+    )
+}
